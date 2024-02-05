85de4fdda3f092c6812ee58e6ffac7a45157b340
maegan <maegan@siena.edu>
2024-02-05 18:03:38 -0500

    put code back to end of day thursday

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({ id, name, email }) => {
   return (
-    <div className="bg-light-green dib br3 ma2 pa3 tc grow">
+    <div className="bg-light-green dib br3 ma2 pa3 tc grow bw2 shadow-5">
       <img
-        src="https://robohash.org/maryanneegan?size=200x200"
+        src={`https://robohash.org/${name}?size=200x200`}
         alt="pic of robot"
       />
       <div>
-        <h2>Jane Doe</h2>
-        <p>jane.doe@gmail.com</p>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
     </div>
   );
