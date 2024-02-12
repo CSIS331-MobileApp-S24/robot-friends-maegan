833cca78664c70ca3af71e78d644497d86d1cfa4
maegan <maegan@siena.edu>
2024-02-12 15:58:02 -0500

    end of 4th lecture

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import React from "react";
 import CardList from "./CardList";
 import { robots } from "./robots";
 import SearchBox from "./SearchBox";
+import Scroll from "./Scroll";
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = { robots: robots, searchfield: "" };
+    this.state = { robots: [], searchfield: "" };
   }
 
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value });
   };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((robot) => {
+        console.log({ robot });
+        this.setState({ robots: robot });
+      });
+  }
 
   render() {
     const filteredRobots = this.state.robots.filter((robot) => {
@@ -22,9 +34,11 @@ class App extends React.Component {
     console.log(filteredRobots);
     return (
       <div className="tc">
-        <h1>RobotFriends</h1>
+        <h1 className="f1">RobotFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={this.state.robots} />
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
       </div>
     );
   }
